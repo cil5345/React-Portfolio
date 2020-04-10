56cae5be54255c74245ce0808162d5f8527ede68
Chris Lee <cil5345@gmail.com>
2020-04-10 09:27:42 -0400

    added a photo

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -8,13 +8,13 @@ function Portfolio() {
       <div class="nav-text">
         <h1>Portfolio</h1>
         <div class="row">
-            <Card href="https://cil5345.github.io/Weather-App/" src="https://via.placeholder.com/150" props="Weather App" description="Weather is very unpredicatable. One minute it can be sunny and then next it can be pouring down
+            <Card href="https://cil5345.github.io/Weather-App/" src={require("../../images/weatherapp.png")} props="Weather App" description="Weather is very unpredicatable. One minute it can be sunny and then next it can be pouring down
                         rain. Luckily my weather app was created to predict the weather 5 days."/>
-            <Card href="https://cil5345.github.io/Weather-App/" src="https://via.placeholder.com/150" props="Weather App" description="Weather is very unpredicatable. One minute it can be sunny and then next it can be pouring down
+            <Card href="https://cil5345.github.io/Weather-App/" src="" props="Weather App" description="Weather is very unpredicatable. One minute it can be sunny and then next it can be pouring down
                         rain. Luckily my weather app was created to predict the weather 5 days."/>
-            <Card href="https://cil5345.github.io/Weather-App/" src="https://via.placeholder.com/150" props="Weather App" description="Weather is very unpredicatable. One minute it can be sunny and then next it can be pouring down
+            <Card href="https://cil5345.github.io/Weather-App/" src="" props="Weather App" description="Weather is very unpredicatable. One minute it can be sunny and then next it can be pouring down
                         rain. Luckily my weather app was created to predict the weather 5 days."/>
-            <Card href="https://cil5345.github.io/Weather-App/" src="https://via.placeholder.com/150" props="Weather App" description="Weather is very unpredicatable. One minute it can be sunny and then next it can be pouring down
+            <Card href="https://cil5345.github.io/Weather-App/" src="" props="Weather App" description="Weather is very unpredicatable. One minute it can be sunny and then next it can be pouring down
                         rain. Luckily my weather app was created to predict the weather 5 days."/>
         </div>
       </div>
